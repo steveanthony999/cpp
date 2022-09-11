3324bf7e3d8df40505421c36991a16e59011a193
Steve <steveanthony999@gmail.com>
2022-09-11 11:42:11 -0700

    cleaned up comments

--- math/gcd.cpp
+++ math/gcd.cpp
@@ -1,4 +1,9 @@
-// The following is an example of using a loop to compute a mathematical quantity. The program computes the greatest common divisor (GCD) among two user-entered integers numA and numB, using Euclid's algorithm: If numA > numB, set numA to numA - numB, else set numB to numB - numA. These steps are repeated until numA equals numB, at which point numA and numB each equal the GCD.
+/* The following is an example of using a loop 
+to compute a mathematical quantity. The program
+computes the greatest common divisor (GCD) among
+two user-entered integers numA and numB, using
+Euclid's algorithm: If numA > numB, set numA to numA - numB, else set numB to numB - numA.
+These steps are repeated until numA equals numB, at which point numA and numB each equal the GCD. */
 
 int main() {
    int numA;
