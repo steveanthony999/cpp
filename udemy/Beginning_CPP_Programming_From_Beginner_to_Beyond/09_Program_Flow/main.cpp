e12bfa2f38a4d63109099315ed05f559dd92b293
Steven Woodward <steveanthony999@gmail.com>
2022-09-28 23:51:26 -0700

    set up program flow skeleton and integrated drawio

---
+++ udemy/Beginning_CPP_Programming_From_Beginner_to_Beyond/09_Program_Flow/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+// Control structures that allow you to control the flow of execution of the program.
+
+// Sequential - Ordering statements sequentially.
+
+// Selection - Making decisions.  Allow you to make decisions and executes parts of your program only when certain conditions are true or false.
+// Selections statements:
+//  if statement
+//  if-else statement
+//  nested if statements
+//  switch statement
+//  Conditional operator ?:
+
+// Iteration - Looping or repeating.  These let you loop and repeat parts of your program.
+// Iteration - Looping constructs:
+//  for loop
+//  Range-based for loop
+//  while loop
+//  do-while loop
+//  continue and break
+//  infinite loops
+//  nested loops
+
+// With these three control structures, you can implement ANY algorithm.
+
+int main() {
+    
+}
